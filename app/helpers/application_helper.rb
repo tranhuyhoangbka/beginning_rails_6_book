843629d193403e687f69c5ae6971504cb4385fdf
tranhuyhoangbka <hoangth92.nd@gmail.com>
2021-09-20 14:02:26 +0700

    Chapter 17: I18n

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,12 @@ module ApplicationHelper
     form.submit + ' or ' + link_to(name, 'javascript:history.go(-1);',
       class: 'cancel')
   end
+
+  def language_selector
+    if I18n.locale == :en
+      link_to 'Pt', url_for(locale: 'pt-br')
+    else
+      link_to 'EN', url_for(locale: 'en')
+    end
+  end
 end
--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,6 @@ module Hello
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    I18n.available_locales = [:en, :'pt-br', :vi]
   end
 end
